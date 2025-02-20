ac6690ae29721adf04a2701475b4f19c8c434a79
sandwhydev <sandwhydev@gmail.com>
2025-02-20 16:10:57 +0700

    test version

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-  res.send("berhasil deploy");
+  res.send("test version");
 });
 
 //ROUTES
